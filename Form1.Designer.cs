a690d91557a8f226f7af4c737f64acac01cc3213
rmmartinez24 <104732814+rmmartinez24@users.noreply.github.com>
2022-05-01 11:51:58 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ActivityFour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SecondConverter = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Amount of Seconds";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(405, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // SecondConverter
+            // 
+            this.SecondConverter.Location = new System.Drawing.Point(405, 182);
+            this.SecondConverter.Name = "SecondConverter";
+            this.SecondConverter.Size = new System.Drawing.Size(100, 23);
+            this.SecondConverter.TabIndex = 2;
+            this.SecondConverter.Text = "Convert";
+            this.SecondConverter.UseVisualStyleBackColor = true;
+            this.SecondConverter.Click += new System.EventHandler(this.SecondConverter_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.SecondConverter);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SecondConverter;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActivityFour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SecondConverter_Click(object sender, EventArgs e)
+        {
+            double minutes;
+            double hours;
+            double days;
+            double seconds;
+
+            seconds = double.Parse(textBox1.Text);
+
+            // Minutes
+            if (seconds >= 60 && seconds <= 3600)
+            {
+                minutes = seconds / 60;
+
+                textBox2.Text = minutes.ToString() + " Minutes";
+            }
+
+            // Hours
+            else if (seconds >= 3600 && seconds <= 86400)
+            {
+                hours = seconds / 3600;
+
+                textBox2.Text = hours.ToString() + " Hours";
+            }
+
+            // Days
+            else if (seconds >= 86400)
+            {
+                days = seconds / 86400;
+
+                textBox2.Text = days.ToString() + " Days";
+            }
+
+            // Seconds
+            else if (seconds <= 59)
+            {
+                textBox2.Text = seconds.ToString() + " Seconds";
+            }
+        }
+    }
+}
